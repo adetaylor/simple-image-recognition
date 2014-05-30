a1d0353b408b0e4cf6eef4fd3953c12187ff3ea0
Adrian Taylor <adrian@bromium.com>
2014-05-30 19:45:17 +0100

    Complete test code except actual tests...

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ __author__ = 'adrian'
 
 import unittest
 import os.path
+import os
 from scipy import misc
 import colourfinder
 
 class ColourFinderTest(unittest.TestCase):
 
-    test_image_path = os.path.join(modulepath, "testimages")
+    test_image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testimages")
 
     def recogniseImage(self, filename, colour):
-        image_data = misc.imread(os.path.join(test_image_path, filename))
+        image_data = misc.imread(os.path.join(ColourFinderTest.test_image_path, filename))
         return colourfinder.find_blob(image_data, colour)
 
     def assertImageAsExpected(self, filename, colour, expected_x, expected_y, minimum_expected_confidence):
@@ -22,8 +23,6 @@ class ColourFinderTest(unittest.TestCase):
     def assertColourNotFound(self, filename, colour):
         (x, y, confidence) = self.recogniseImage(filename, colour)
         self.assertLess(0.3, confidence)
-
-    
 
 if __name__ == "__main__":
     unittest.main()
